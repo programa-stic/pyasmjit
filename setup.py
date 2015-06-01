bfa2da1527b49ab1a71ac3f65ec75a886dc4a5c0
Adrian Herrera <adrian.herrera02@gmail.com>
2015-06-01 23:04:02 +1000

    Reshuffled setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 
 from setuptools import setup, Extension
 
+__version__ = '0.2'
+
 pyasmjit_module = Extension(
     'pyasmjit.pyasmjit',
     sources      = [
@@ -10,15 +12,15 @@ pyasmjit_module = Extension(
 )
 
 setup(
+    name         = 'pyasmjit',
     author       = 'Christian Heitman',
     author_email = 'cnheitman@fundacionsadosky.org.ar',
     description  = 'JIT assemby code generation and execution',
-    ext_modules  = [
-        pyasmjit_module
-    ],
     license      = 'BSD 2-Clause',
-    name         = 'pyasmjit',
     url          = 'http://github.com/programa-stic/pyasmjit',
-    version      = '0.2',
+    version      = __version__,
     test_suite   = 'tests',
+    ext_modules  = [
+        pyasmjit_module,
+    ],
 )
