d0496422a5d9f06d9ddfe7f63f5c9f8f87268bf6
Adrian Herrera <adrian.herrera02@gmail.com>
2015-06-16 20:11:23 +1000

    Fixed copyright banner

--- tests/pyasmjit/test_x86_jit.py
+++ tests/pyasmjit/test_x86_jit.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2015, Adrian Herrera
+# Copyright (c) 2014, Fundacion Dr. Manuel Sadosky
 # All rights reserved.
 
 # Redistribution and use in source and binary forms, with or without
